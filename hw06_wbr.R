bad5fa568ae13093cd1d37062c44ea3ef91fce41
Walter Reilly <wbreilly@ucdavis.edu>
2016-11-18 10:05:09 -0800

    finished mostly homework 6

--- hw06_wbr.R
+++ hw06_wbr.R
@@ -2,6 +2,29 @@
 # 11.17.16
 # HW 6
 
-setwd("walter/204")
+setwd("walter/204_stats")
  data = read.csv("hw06data.csv")
 d = data.frame(data)
+
+library(tidyverse)
+
+# count n by factor combinations
+d %>%
+  group_by(biosex, workout) %>%
+  count()
+
+# summary stats - grand mean
+sums1 = summarise(d, mean=mean(pctgain), sd=sd(pctgain))
+
+# summary stats - main effect biosex
+sums2 = summarise(group_by(d, biosex),
+          mean=mean(pctgain), sd=sd(pctgain))
+
+# summary stats - interactions
+sums3 = summarise(group_by(d, biosex, workout),
+         mean=mean(pctgain), sd=sd(pctgain))
+
+# factorial anova
+anova(lm(d$pctgain ~ d$biosex + d$workout + d$biosex*d$workout))
+
+
